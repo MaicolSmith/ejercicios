705b8dc0484ada51b887fa3f5a01f692a02127ad
MaicolSmith <maicol.smith.bueno@gmail.com>
2024-04-03 20:59:25 -0500

    ejercicio1

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,6 +1,14 @@
 //Generar un código que determine si un numero es par o impar
+console.log ("-----ejercicio 1---");
+const numero = 6;
+if ( numero % 2==0 ) {
+    console.log ("el numero es par");
+} else {
+    console.log ("el numero es impar");
+}
 
 
+  
 
 
 
