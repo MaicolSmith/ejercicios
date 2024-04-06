66b3081ac440e53b6e9f0e2016377acaf7cab606
MaicolSmith <maicol.smith.bueno@gmail.com>
2024-04-05 20:54:56 -0500

    ejercicio 5 terminado

--- ejercicio5/ejercicio5.js
+++ ejercicio5/ejercicio5.js
@@ -1,1 +1,10 @@
-console.log("----ejercicio 5-----")
+console.log("----ejercicio 5-----");
+let kilometros=220;
+let tiempo=5;
+let total = kilometros/tiempo ;
+
+if (total > 40 & total < 60) {
+ console.log(" aprueba con la velocidad " ,total);
+} else {
+ console.log(" descalificado ",total);
+}
