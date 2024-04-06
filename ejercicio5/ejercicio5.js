ebed812d5af4bdb8e87315d37d00cc4535804986
MaicolSmith <maicol.smith.bueno@gmail.com>
2024-04-05 21:02:00 -0500

    complemento y solicion ejercicio5

--- ejercicio5/ejercicio5.js
+++ ejercicio5/ejercicio5.js
@@ -1,3 +1,9 @@
+// En un tramo de un really los conductores no deben ir ni demasiado
+//rapido ni demasiado lento. Este ejercicio debe tomar la longitud del tramo en
+//kilometros y el tiempo empleado, si la velocidad está entre
+//40 y 60 km/h el conductor pasa la prueba en caso contrario es descalificado.
+//Ejemplo. si recorre 100 km en 4 horas, ha ido a una velocidad de 100/4 que son 25km/h. Estaría descalificado.
+
 console.log("----ejercicio 5-----");
 let kilometros=220;
 let tiempo=5;
